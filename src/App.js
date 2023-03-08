bf82fc5da41d35495407c5aa8b5364fc92e42ad4
masonmike2001 <masonmike2001@gmail.com>
2023-03-08 10:18:34 -0500

    styled css of CV

--- src/App.js
+++ src/App.js
@@ -11,10 +11,16 @@ class App extends Component {
     this.state = {
       name: "Gizmo Gecko",
       email: "gizmo@geicko.com",
+      summary:
+        "As a leopard gecko working as an insurance salesgecko extroardinaire, I may not have the ability to save you 15% or more on car insurance, but I can certainly make the process more enjoyable. I may be small, but I have a big personality and a passion for insurance (yes, really!).",
       phone: "(508) 324-5906",
       schoolName: "University of Massachusetts",
       schoolField: "B.S. in Geckommunications",
-      studyRange: { schoolStart: new Date(), schoolEnd: "", isAttending: "" },
+      studyRange: {
+        schoolStart: [2019, 9, 1],
+        schoolEnd: [2019, 12, 25],
+        isAttending: false,
+      },
       workName: "Geicko",
       workTitle: "Insurance Salesgecko Extraordinaire",
       workTask: { text: "", id: uniqid() },
@@ -30,24 +36,26 @@ class App extends Component {
 
   render() {
     const name = this.state.name;
+    const summary = this.state.summary;
     const email = this.state.email;
     const phone = this.state.phone;
     const schoolName = this.state.schoolName;
     const schoolField = this.state.schoolField;
+    const studyRange = this.state.studyRange;
     const workName = this.state.workName;
     const workTitle = this.state.workTitle;
     const workTask = this.state.workTask;
-    // const studyRange = this.props.studyRange;
     return (
       <div id="holder">
         <div id="left">
           <Form
             name={name}
+            summary={summary}
             email={email}
             phone={phone}
             schoolName={schoolName}
             schoolField={schoolField}
-            // studyRange={studyRange}
+            studyRange={studyRange}
             workName={workName}
             workTitle={workTitle}
             workTask={workTask}
@@ -61,11 +69,12 @@ class App extends Component {
         <div id="right">
           <CV
             name={name}
+            summary={summary}
             email={email}
             phone={phone}
             schoolName={schoolName}
             schoolField={schoolField}
-            // studyRange={studyRange}
+            studyRange={studyRange}
             workName={workName}
             workTitle={workTitle}
             workTask={workTask}
--- src/components/CV.js
+++ src/components/CV.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import "./CV.css";
+import logo from "../assets/logo.png";
 
 class CV extends Component {
   constructor(props) {
@@ -6,42 +8,73 @@ class CV extends Component {
   }
   render() {
     const name = this.props.name;
+    const summary = this.props.summary;
     const email = this.props.email;
     const phone = this.props.phone;
     const schoolName = this.props.schoolName;
     const schoolField = this.props.schoolField;
+    const studyRange = this.props.studyRange;
     const workName = this.props.workName;
     const workTitle = this.props.workTitle;
     const workTask = this.props.workName;
-    // const studyRange = this.props.studyRange;
 
     return (
       <div>
-        <div>
-          <h1>{name}</h1>
-          <h3>{email}</h3>
-          <h3>{phone}</h3>
-        </div>
-        <hr />
-        <div>
-          <h4>Experience:</h4>
+        <div id="profile-head">
+          <div>
+            <h1>{name}</h1>
+            <img src={logo} className="logo"></img>
+          </div>
+
+          <p
+            className="
+          alt-color"
+          >
+            Contact
+          </p>
           <ul>
             <li>
-              <h3>
-                {workTitle} at {workName}
-              </h3>
+              <h3>{email}</h3>
             </li>
-          </ul>
-        </div>
-        <div>
-          <h4>Education:</h4>
-          <ul>
             <li>
-              <h3>
-                {schoolName} ({schoolField})
-              </h3>
+              <h3>{phone}</h3>
             </li>
           </ul>
+          <p
+            className="
+          alt-color"
+          >
+            Summary
+          </p>
+          <h4 class="list">{summary}</h4>
+        </div>
+        <div id="CV-body">
+          <div>
+            <h4>Experience:</h4>
+            <ul>
+              <li>
+                <h3>
+                  {workTitle} at {workName}
+                </h3>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h4>Education:</h4>
+            <ul>
+              <li>
+                <h3>
+                  {schoolName} ({schoolField}):
+                </h3>
+                <p>
+                  {" "}
+                  {studyRange.schoolStart[1]}/{studyRange.schoolStart[2]}/
+                  {studyRange.schoolStart[0]} - {studyRange.schoolEnd[1]}/
+                  {studyRange.schoolEnd[2]}/{studyRange.schoolEnd[0]}
+                </p>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     );
